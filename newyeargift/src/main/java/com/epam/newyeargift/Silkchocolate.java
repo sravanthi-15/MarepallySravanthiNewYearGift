6d028b960479b57d73987ec447254c49a323dba4
P Anjali <P Anjali@192.168.0.104>
2020-02-28 20:24:28 +0530

    different chocolate classes are created by implementing Chocolate interface

---
+++ newyeargift/src/main/java/com/epam/newyeargift/Silkchocolate.java
@@ -0,0 +1,11 @@
+package com.epam.newyeargift;
+
+public class Silkchocolate  implements Chocolates{
+	double weightOfChoc = 60;
+
+	public double calcWeight(int numOfChoc) {
+		// TODO Auto-generated method stub
+		return numOfChoc * weightOfChoc;
+	}
+
+}
